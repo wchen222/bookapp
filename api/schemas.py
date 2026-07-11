fb00d0fe360d01efe13c342f2b0cb5d2648b8e74
Wilson <wchen222@buffalo.edu>
2026-07-10 20:22:34 -0400

    - seed.py to insert 266,737 clean book records into database

--- api/schemas.py
+++ api/schemas.py
@@ -15,7 +15,7 @@ class UserCreate(UserBase):
 
 class BookBase(BaseModel):
     isbn: str = Field(min_length=1, max_length=100)
-    title: str = Field(min_length=1, max_length=100)
+    title: str = Field(min_length=1, max_length=200)
     author: str = Field(min_length=1, max_length=50)
     year: int
 
---
+++ seed.py
@@ -0,0 +1,38 @@
+import csv
+import asyncio
+import api.models as models
+from api.database import AsyncSessionLocal
+
+BOOK_CSV_PATH = "./data/books_clean.csv"
+BATCH_SIZE = 5000
+
+async def seed_books():
+    async with AsyncSessionLocal() as db:
+        books_batch = []
+
+        with open(BOOK_CSV_PATH, mode="r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+
+            for count, book in enumerate(reader, start=1):
+                new_book = models.Book(
+                    isbn=book["ISBN"],
+                    title=book["Book-Title"],
+                    author=book["Book-Author"],
+                    year=int(book["Year-Of-Publication"]),
+                )
+                books_batch.append(new_book)
+
+                if len(books_batch) >= BATCH_SIZE:
+                    db.add_all(books_batch)
+                    await db.flush()
+                    books_batch = []
+                    print(f"Pushed {count} books...")
+
+            if books_batch:
+                db.add_all(books_batch)
+                print(f"Pushed all leftover books.")
+
+        await db.commit()
+
+if __name__ == "__main__":
+    asyncio.run(seed_books())
